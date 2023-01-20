671f017920a7f3741a0e4570574a25a70f28bc18
weyrhvwvrwuvureurw <123091788+weyrhvwvrwuvureurw@users.noreply.github.com>
2023-01-20 07:25:02 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,5 +24,38 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    layerShown(){return true},
+    11: {
+    title: "Double your points gain.",
+    description: "Double your points gain.",
+    cost: new Decimal(25),
+    },
+})
+addLayer("i", {
+    name: "insanity", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "I", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(1500),
+    }},
+    color: "#FFA600",
+    requires: new Decimal(1650), // Can be a function that takes requirement increases into account
+    resource: "insanity", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.6, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(6)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1.012)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "i", description: "I: Reset for insanity.", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
     layerShown(){return true}
 })
