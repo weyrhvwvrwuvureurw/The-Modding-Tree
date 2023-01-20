da9312c8a04d4210631620cc826f0d71fdaa1aba
weyrhvwvrwuvureurw <123091788+weyrhvwvrwuvureurw@users.noreply.github.com>
2023-01-20 07:50:10 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,7 +14,7 @@ addLayer("p", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+        mult = new Decimal(2)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -25,18 +25,13 @@ addLayer("p", {
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
-    11: {
-        title: "Double your points gain.",
-        description: "Double your points gain.",
-        cost: new Decimal(1),
-       },
-    12: {
-        title: "Double your points gain.",
-        description: "Double your points gain.",
-        cost: new Decimal(1),
-       },
-})
-
+    upgrades: {
+        11: {
+    title: "Double your points gain.",
+    description: "Double your points gain."
+    cost: new Decimal(1)
+    })
+ 
 addLayer("i", {
     name: "insanity", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "I", // This appears on the layer's node. Default is the id with the first letter capitalized
@@ -48,8 +43,8 @@ addLayer("i", {
     color: "#FFA600",
     requires: new Decimal(1650), // Can be a function that takes requirement increases into account
     resource: "insanity", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseResource: "prestige points", // Name of resource prestige is based on
+    baseAmount() {return prestige.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.6, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
