3f81e0cbcd8470ccb9e5889c488a3fef58800f77
weyrhvwvrwuvureurw <123091788+weyrhvwvrwuvureurw@users.noreply.github.com>
2023-02-10 20:33:14 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
 	name: "The ??? Tree",
-	id: "8532943",
+	id: "844",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
@@ -44,8 +44,9 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if (hasUpgrade('p', 11)) gain = gain.times(2);
 	if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12));
-}
 	return gain
+}
+	
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
 function addedPlayerData() { return {
