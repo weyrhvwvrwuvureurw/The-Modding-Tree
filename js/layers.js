bf1216373be0327035bcd6427e3a9e321e8f1801
weyrhvwvrwuvureurw <123091788+weyrhvwvrwuvureurw@users.noreply.github.com>
2023-02-11 11:46:38 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -75,7 +75,7 @@ addLayer("i", {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.4, // Prestige currency exponent
+    exponent: 0.1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(6)
         return mult
